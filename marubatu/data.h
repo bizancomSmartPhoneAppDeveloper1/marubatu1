ee137563ac0890d5c45afa6575148494e92d6352
nakanishi yuuki <golugo134@yahoo.co.jp>
2014-04-04 15:52:29 +0900

    dd

---
+++ marubatu/data.h
@@ -0,0 +1,15 @@
+//
+//  data.h
+//  marubatu
+//
+//  Created by bizan.com.mac06 on 2014/04/04.
+//  Copyright (c) 2014年 bizan.com.mac04. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface data : NSObject
+@property int  preserve;
+-(void)setInteger:(NSInteger)value
+forKey:(NSString *)defaultName;
+@end
